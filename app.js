f9ca06973c7704fbf395128ac3e6430428b7961e
tinuola <tinuola@users.noreply.github.com>
2017-07-09 00:07:55 -0700

    add third route

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 // libraries that this app needs
 var express = require('express');
+var path = require('path');
 
 // initialize app
 var app = express();
 
 // set the type of template that the app will use
 app.set('view engine', 'ejs');
-
 // set the directory for the templates
 app.set('views', path.join(__dirname, 'views'));
 
-// display root route
+// display root route - landing page
 app.get('/', function (request, response) {
+  response.render('welcome');
+});
+
+// display login page
+app.get('/login', function (request, response) {
   response.render('login');
 });
 
-// display login route
-app.get('/', function (request, response) {
-  response.render('login');
+//display dashboard
+app.get('/dashboard', function (request, response) {
+  response.render('dashboard');
 });
 
 // start server on port

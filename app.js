6f097e9ad8a7d19c9ccfb0415e203822137b152e
tinuola <tinuola@users.noreply.github.com>
2017-07-11 01:10:49 -0700

    updated app.js and welcome.ejs files, created layout.ejs

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // libraries that this app needs
 var express = require('express');
 var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
 
 // initialize app
 var app = express();
 
+
+// add layout support
+app.use(expressLayouts);
+// specifiy location of layout file
+app.set('layout', path.join(__dirname, 'views', 'layouts', 'layout'));
+
+
 // set the type of template that the app will use
 app.set('view engine', 'ejs');
+
 // set the directory for the templates
 app.set('views', path.join(__dirname, 'views'));
+
+// set the folder for  static assets
+app.use(express.static(path.join(__dirname, 'public')));
 
 // display root route - landing page
 app.get('/', function (request, response) {

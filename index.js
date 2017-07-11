06dcaa0f02232e9cf1fb1bdcdb98e0c4d10066ff
itsme <lfifteena@gmail.com>
2017-07-11 00:55:42 -0700

    draft of fake data

--- index.js
+++ index.js
@@ -1,1 +1,153 @@
 //I'm in the branch "fakeDatabase"
+//
+
+// Fake API DATA for USER
+
+var user = [
+    {
+        name: "Alice",
+        email: "crazycatlady@yahoo.com",
+        tapCard: "1743528521322620000",
+    },
+    {
+        name: "Alex",
+        email: "hotrodalex974@hotmail.com",
+        tapCard: "1183812665105740000",
+    },
+    {
+        name: "Mike",
+        email: "mikeshotdiggdydogs@gmail.com",
+        tapCard: "1230399606150920000",
+    },
+    {
+        name: "Jason",
+        email: "yodayogis247@gmail.com",
+        tapCard: "1180503862912550000",
+    },
+    {
+        name: "Candice",
+        email: "candycane2392@gmail.com",
+        tapCard: "1371565217052500000",
+    },
+];
+
+// Fake API DATA for REWARDS
+var reward = [
+    {
+        rewardType: "One Free Metro Ride"
+        pointsRequired: "5"
+        icon:
+    },
+    {
+        rewardType: "One Week Free Metro Ride"
+        pointsRequired: "10"
+        icon:
+    },
+        rewardType: "One Free Lyft/Uber Ride (under 10 miles)"
+        pointsRequired: "15"
+        icon:
+    },
+    {
+        rewardType: "Free Month of Car Parking"
+        pointsRequired: "20"
+        icon:
+    },
+    {
+        rewardType: "Free Month of Bike Storage"
+        pointsRequired: "20"
+        icon:
+    }
+];
+
+
+
+
+ +
+ +var vendors = [
+ +	{
+ +		name: "Chipotle",
+ +		reward: "Free Burrito",
+ +		pointsNeeded: 150,
+ +		imgURL: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1024px-Chipotle_Mexican_Grill_logo.svg.png"
+ +	},
+ +	{
+ +		name: "Dominos",
+ +		reward: "Free Breadsticks",
+ +		pointsNeeded: 100,
+ +		imgURL: "https://www.festisite.com/static/partylogo/img/logos/dominos_pizza.png"
+ +	},
+ +	{
+ +		name: "Menchie's",
+ +		reward: "Free Small Cup",
+ +		pointsNeeded: 250,
+ +		imgURL: "https://works-progress.com/wp-content/uploads/2015/12/menchies.png"
+ +	}
+ +];
+ +
+ +
+
+
+
+
+
+
+
+
+//
+//
+//  +// ******* Wai-Yin's ********
+// // exports.users = [
+// //   { id: 1, name: "Amy", team_id: 1 },
+// //   { id: 2, name: "Bridget", team_id: 2 },
+// //   { id: 3, name: "Carolina", team_id: 2 },
+// //   { id: 4, name: "Christa", team_id: 1 },
+// //   { id: 5, name: "Cindy", team_id: 2 }
+// // ];
+// //
+// // exports.teams = [
+// //   { id: 1, name: "Team One" },
+// //   { id: 2, name: "Team Two" },
+// // ]
+//
+//
+//  +// ******* SEED DATA ********
+//  +
+//  +var user = [
+//  +	{
+//  +		first: "John",
+//  +		last: "Bello",
+//  +		email: "a@b.com",
+//  +		pointsBalance: 697,
+//  +		tapNum: 1234567891012131
+//  +	},
+//  +	{
+//  +		first: "Erica",
+//  +		last: "Cerulo",
+//  +		email: "e@b.com",
+//  +		pointsBalance: 253,
+//  +		tapNum: 7649683429652100
+//  +	}
+//  +];
+//  +
+//  +var vendors = [
+//  +	{
+//  +		name: "Chipotle",
+//  +		reward: "Free Burrito",
+//  +		pointsNeeded: 150,
+//  +		imgURL: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1024px-Chipotle_Mexican_Grill_logo.svg.png"
+//  +	},
+//  +	{
+//  +		name: "Dominos",
+//  +		reward: "Free Breadsticks",
+//  +		pointsNeeded: 100,
+//  +		imgURL: "https://www.festisite.com/static/partylogo/img/logos/dominos_pizza.png"
+//  +	},
+//  +	{
+//  +		name: "Menchie's",
+//  +		reward: "Free Small Cup",
+//  +		pointsNeeded: 250,
+//  +		imgURL: "https://works-progress.com/wp-content/uploads/2015/12/menchies.png"
+//  +	}
+//  +];
+//  +
+//  +

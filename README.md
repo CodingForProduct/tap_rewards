ed66a24a2b8ab6aa5f7b2d5939b1ef7fed509937
tinuola <tinuola@users.noreply.github.com>
2017-07-12 23:58:30 -0700

    cleaning up master, adding new routes, updating with hardcoded data

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-# JavaScripters
-LA Metro Project - Beginning JavaScript Group
+# TAP REWARDS 
+
+Coding for Product - LA Metro Project
 <br><br>
 Project Purpose:
 Point system tied to TAP card use to reward ridership
 Rewards encourage/discount further metro use
 <br><br>
-Team Members (javascript - beginner team):<br>
+Team: JAVASCRIPTERS
+<br><br>
+Team Members (JavaScript Beginner Team):<br>
     Cindy Wang<br>
     Tinuola Awopetu<br>
     Carolina Bravo<br>
     Lisa<br>
 <br><br>
 Google Docs project planning document - https://docs.google.com/document/d/1SLUl1XAlJxG8Rjg48cc9j45sVwlbJvwXDWtVRv-Exuc/edit
-<br>
+<br><br>
 To start the app ==> npm run dev
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
+var data = require('./data/rider_data');
 
 // initialize app
 var app = express();
@@ -9,9 +10,9 @@ var app = express();
 
 // add layout support
 app.use(expressLayouts);
+
 // specifiy location of layout file
 app.set('layout', path.join(__dirname, 'views', 'layouts', 'layout'));
-
 
 // set the type of template that the app will use
 app.set('view engine', 'ejs');
@@ -27,14 +28,34 @@ app.get('/', function (request, response) {
   response.render('welcome');
 });
 
+// display signup page
+app.get('/signup', function (request, response) {
+  response.render('signup');
+});
+
 // display login page
 app.get('/login', function (request, response) {
   response.render('login');
 });
 
-//display dashboard
+// array of riders and rewards
+var metroRiders = data.rider;
+var metroRewards = data.reward;
+
+// display dashboard - with rider data
 app.get('/dashboard', function (request, response) {
-  response.render('dashboard');
+	// pass data to template
+	response.render('dashboard', {
+		rider: metroRiders
+	});
+});
+
+// display rewards page - reward options data
+app.get('/rewards', function (request, response) {
+ // pass data to template
+	response.render('rewards', {
+		reward: metroRewards
+	});
 });
 
 // start server on port

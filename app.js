0335d0f71b0701d00eeecdaf9ccdbbf9c2d40fe2
tinuola <tinuola@users.noreply.github.com>
2017-07-28 03:58:38 -0700

    dashboard calculations functioning

--- app.js
+++ app.js
@@ -56,74 +56,39 @@ app.get('/resetpw', function (request, response) {
 // display dashboard - with rider data and rewards
 app.get('/dashboard', function (request, response) {
 	// pass data to template
-  var riders = db.get('riders').value()
-  var rewards = db.get('rewards').value()
-  response.render('dashboard', {riders: riders, rewards: rewards})
+  var rider = db.get('riders').find({ name: 'Alice Green' }).value()
+  var reward = db.get('rewards').value()
+  response.render('dashboard', {rider: rider, reward: reward })
 });
-
-// create a new reward
-// app.post('/createReward', function(req, res) {
-  // get data from form
-  // var rewardType = req.body.rewardType;
-  // var pointsRequired = req.body.pointsRequired;
-
-  // insert new reward into database
-    // db.get('rewards')
-    //   .push({rewardType: rewardType, pointsRequired: pointsRequired, id: uuid()})
-    //   .write()
-
-  // redirect
-  //     res.redirect('redeem')
-  // })
 
 // Select reward from form, update balance
 // Redirect to redeem page to display redemption and new balance message
   app.post('/redeem', function(request, response) {
+    console.log(request.body)
     // get data from form/dashboard page
-    // var rewardType = request.body.rewardType;
-    // var pointsRequired = request.body.pointsRequired;
-    // var id = request.params.id;
-    // var newBalance = pointBalance - pointsRequired;
-
+    var pointsMinus = request.body.pointsRequired;
+    var currentBalance = request.body.pointBalance;
     //update user balance
     db.get('riders')
       .find({ name: 'Alice Green' })
-      .assign({ pointBalance: 550-20 })
+      .assign({ pointBalance: currentBalance - pointsMinus })
       .write()
 
 // redirect
   response.redirect('redeem')
 });
 
-// display one reward on redeem page, using ":id" - works for linked list, but not for Form submission; but not able to display rider name and points on redeem page
-  app.get('/dashboard/:id', function(request, response) {
+// display one reward on redeem page, using ":id"
+  // app.get('/redeem/:id', function(request, response) {
+  app.get('/redeem/:id', function(request, response) {
     var reward = db.get('rewards').find({ id: request.params.id }).value()
     var points;
-    var rider = db.get('riders').find({ name: request.params.name }).value()
-    var balance;
+    var rider = db.get('riders').find({ name: 'Alice Green' }).value()
     if(reward) {
       points = db.get('rewards').find({ id: reward.pointsRequired }).value()
     }
-    // if(balance) {
-    //   balance = db.get('riders').find({ name: request.params.pointBalance }).value()
-    // }
-    response.render('redeem', { reward: reward || {}, points: points || {}, rider: rider || {}, balance: balance || {}})
+    response.render('redeem', { reward: reward || {}, points: points || {}, rider: rider || {}})
   })
-
-// display one reward on redeem page - seems to only work for Form submission, but not able to add "/:id" to display each reward and its points on the redeem page; also not showing rider name and points on the redeem page
-  app.get('/redeem', function(request, response){
-    var reward = db.get('rewards').find({ id: request.params.id }).value()
-    var points;
-    var rider = db.get('riders').find({ name: request.params.name }).value()
-    var balance;
-    if(reward) {
-      points = db.get('rewards').find({ id: reward.pointsRequired }).value()
-    }
-    // if(balance) {
-    //   balance = db.get('riders').find({ name: request.params.pointBalance }).value()
-    // }
-    response.render('redeem', { reward: reward || {}, points: points || {}, rider: rider || {}, balance: balance || {}})
- });
 
 // start server on port
 app.listen(3000, function() {
